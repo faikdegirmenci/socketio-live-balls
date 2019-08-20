8a78699cf4c033096165c722284144455a588f0f
Faik Değirmenci <faikdegirmenci@hotmail.com>
2019-08-20 15:45:21 +0300

    newUser

--- src/socketApi.js
+++ src/socketApi.js
@@ -2,11 +2,26 @@ const socketio = require('socket.io');
 const io = socketio();
 
 const socketApi = { };
-
 socketApi.io = io;
+
+const users= [ ];
 
 io.on('connection', (socket)=>{
     console.log('a user connected');
+
+    socket.on('newUser', (data)=>{
+        const defaultData = {
+            id: socket.id,
+            position: {
+                x:0,
+                y:0
+            }
+        }
+
+        const userData = Object.assign(data,defaultData);// assign birleştirme işlemini yapıyor.
+        users.push(userData);
+        console.log(users);
+    });
 });
 
 module.exports = socketApi;
